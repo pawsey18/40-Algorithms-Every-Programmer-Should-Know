3ec3e0cd160a284c2c37c404eadb293ff6edc6fd
Nick Pawsey <pawsey18@gmail.com>
2021-09-12 09:22:30 -0300

    Create NumPy array

---
+++ Chapter02/.ipynb_checkpoints/numpy_arrays_basics.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+print(np.__version__)
+
+my_list = [-17, 0,4,5,9]
+my_array_list_from_list = np.array(my_list)
+my_array_list_from_list
+
+my_array_list_from_list * 10
+
+my_tuple = (4,-3.45,5+7j)
+my_tuple
+my_array_from_tuple = np.array(my_tuple)
+my_array_from_tuple
